9eec43c709c8c70bc51560b800bc5f0e00300bc4
dumch <arturdumchev@yandex.ru>
2025-08-07 20:07:13 +0300

    Run test manually

--- src/test/kotlin/giga/GigaVoiceApiTest.kt
+++ src/test/kotlin/giga/GigaVoiceApiTest.kt
@@ -2,29 +2,23 @@ package giga
 
 import com.dumch.giga.GigaAuth
 import com.dumch.giga.GigaVoiceAPI
-import kotlinx.coroutines.runBlocking
 import java.io.File
-import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class GigaVoiceApiTest {
-    @Test
-    fun `synthesize small text`() = runBlocking {
-        val key = System.getenv("VOICE_KEY")
-        if (key.isNullOrBlank()) {
-            println("VOICE_KEY not set; skipping real API test")
-            return@runBlocking
-        }
+suspend fun main () {
+    val key = System.getenv("VOICE_KEY")
+    if (key.isNullOrBlank()) {
+        println("VOICE_KEY not set; skipping real API test")
+        return
+    }
 
-        val api = GigaVoiceAPI(GigaAuth)
-        try {
-            val audio = api.synthesize("<speak>Hello</speak>")
-            assertTrue(audio.isNotEmpty())
-            val vaw = File("Generated.wav")
-            vaw.writeBytes(audio)
-        } finally {
-            api.clear()
-        }
+    val api = GigaVoiceAPI(GigaAuth)
+    try {
+        val audio = api.synthesize("<speak>Hello</speak>")
+        assertTrue(audio.isNotEmpty())
+        val vaw = File("Generated.wav")
+        vaw.writeBytes(audio)
+    } finally {
+        api.clear()
     }
 }
