ae69326390c85e03b8f2398fcbdab066f797950a
dumch <arturdumchev@yandex.ru>
2025-08-10 08:09:06 +0300

    Fix error handling

--- src/main/kotlin/giga/GigaAgent.kt
+++ src/main/kotlin/giga/GigaAgent.kt
@@ -25,36 +25,25 @@ class GigaAgent(
     private val functions: List<GigaRequest.Function> = tools.map { it.value.fn }
 
     fun run(): Flow<String> = channelFlow {
-        val conversation = ArrayList<GigaRequest.Message>().apply {
+        val conversation = ArrayDeque<GigaRequest.Message>().apply {
             add(systemPrompt)
         }
-        var awaitConfirmation = false
 
         userMessages.collect { userText ->
-            if (awaitConfirmation) {
-                val answer = userText.trim().lowercase()
-                if (!answer.startsWith("y")) {
-                    send("Stopping conversation")
-                    close()
-                }
-                awaitConfirmation = false
-                return@collect
-            }
-
             conversation.add(GigaRequest.Message(GigaMessageRole.user, userText))
             for (i in 1..10) { // infinite loop protection
                 if (!isActive) break
                 val response: GigaResponse.Chat = try {
                     withContext(Dispatchers.IO) { chat(conversation) }
-                } catch (t: Throwable) {
-                    send("Error: ${t.message}. Continue? (y/n)")
-                    awaitConfirmation = true
+                } catch (e: Throwable) {
+                    l.error("Error: ${e.message}", e)
+                    send("Ошибочка вышла, разработчики уже работают над ней. Попробуйте повторить запрос позже.")
                     break
                 }
                 when (response) {
                     is GigaResponse.Chat.Error -> {
-                        send("${response.message}. Continue? (y/n)")
-                        awaitConfirmation = true
+                        l.error("Error: ${response.message}")
+                        send("Не смогли достучаться до сервера. Будем пробовать снова?")
                         break
                     }
 
@@ -62,9 +51,8 @@ class GigaAgent(
                         conversation.addAll(response.toRequestMessages())
                         try {
                             trySummarize(response, conversation)
-                        } catch (t: Throwable) {
-                            send("Error: ${t.message}. Continue? (y/n)")
-                            awaitConfirmation = true
+                        } catch (e: Throwable) {
+                            send("Error: ${e.message}. Continue? (y/n)")
                             break
                         }
 
@@ -87,31 +75,32 @@ class GigaAgent(
                             conversation.addAll(toolAwaits.awaitAll())
                         } catch (t: Throwable) {
                             send("Error: ${t.message}. Continue? (y/n)")
-                            awaitConfirmation = true
                             break
                         }
                     }
                 }
-                if (awaitConfirmation) break
             }
         }
     }
 
-    private suspend fun trySummarize(response: GigaResponse.Chat.Ok, conversation: ArrayList<GigaRequest.Message>) {
+    private suspend fun trySummarize(response: GigaResponse.Chat.Ok, conversation: ArrayDeque<GigaRequest.Message>) {
         val modelContextWindow = GigaModel.entries.first { response.model.startsWith(it.alias) }.maxTokens
         val smallConversation = response.usage.totalTokens < modelContextWindow * SUMMARIZE_THRESHOLD
         if (smallConversation) return
 
-        val response: GigaResponse.Chat = withContext(Dispatchers.IO) {
+        val summaryResponse: GigaResponse.Chat = withContext(Dispatchers.IO) {
             conversation.add(GigaRequest.Message(
                 role = GigaMessageRole.user,
                 content = "Summarize the conversation so far",
             ))
             chat(conversation)
         }
-        val msg: GigaRequest.Message = when(response) {
-            is GigaResponse.Chat.Error -> throw CancellationException("Can't summarize the conversation")
-            is GigaResponse.Chat.Ok -> response.toRequestMessages().last()
+        val msg: GigaRequest.Message = when(summaryResponse) {
+            is GigaResponse.Chat.Error -> {
+                l.error("Error on summarization: ${summaryResponse.message}")
+                return trySummarizaWithLess(conversation, response)
+            }
+            is GigaResponse.Chat.Ok -> summaryResponse.toRequestMessages().last()
         }
         l.info("Summarizing the conversation... $msg")
         val lastMsg = conversation.last()
@@ -119,6 +108,16 @@ class GigaAgent(
         conversation.add(systemPrompt)
         conversation.add(msg)
         conversation.add(lastMsg)
+    }
+
+    private suspend fun trySummarizaWithLess(
+        conversation: ArrayDeque<GigaRequest.Message>,
+        response: GigaResponse.Chat.Ok
+    ) {
+        conversation.removeFirst() // Remove system prompt
+        conversation.removeFirst() // Remove first user message
+        conversation.removeLast() // Summarization request message
+        trySummarize(response, conversation)
     }
 
     private fun GigaResponse.Chat.Ok.toRequestMessages(): Collection<GigaRequest.Message> {
@@ -149,7 +148,7 @@ class GigaAgent(
         return fn.invoke(functionCall)
     }
 
-    private suspend fun chat(conversation: ArrayList<GigaRequest.Message>): GigaResponse.Chat {
+    private suspend fun chat(conversation: ArrayDeque<GigaRequest.Message>): GigaResponse.Chat {
         val body = GigaRequest.Chat(
             messages = conversation,
             functions = functions,
