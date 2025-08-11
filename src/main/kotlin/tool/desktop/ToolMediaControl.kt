14bcb8767df21374be3c328748dd110e5b4c6f17
Artur Dumchev <arturdumchev@yandex.ru>
2025-08-11 14:07:15 +0300

    Add media control tool using AppleScript

---
+++ src/main/kotlin/tool/desktop/ToolMediaControl.kt
@@ -0,0 +1,32 @@
+package com.dumch.tool.desktop
+
+import com.dumch.tool.InputParamDescription
+import com.dumch.tool.ToolRunBashCommand
+import com.dumch.tool.ToolSetup
+
+class ToolMediaControl(private val bash: ToolRunBashCommand) : ToolSetup<ToolMediaControl.Input> {
+    override val name: String = "MediaControl"
+    override val description: String = "Controls media playback, volume, and display brightness"
+
+    override fun invoke(input: Input): String {
+        val script = when (input.action) {
+            "next" -> """tell application \"Music\" to next track"""
+            "previous" -> """tell application \"Music\" to previous track"""
+            "play" -> """tell application \"Music\" to play"""
+            "pause" -> """tell application \"Music\" to pause"""
+            "playpause" -> """tell application \"Music\" to playpause"""
+            "volume_up" -> """set volume output volume ((output volume of (get volume settings)) + 10)"""
+            "volume_down" -> """set volume output volume ((output volume of (get volume settings)) - 10)"""
+            "brightness_up" -> """tell application \"System Events\" to key code 144"""
+            "brightness_down" -> """tell application \"System Events\" to key code 145"""
+            else -> throw IllegalArgumentException("Unknown action: ${input.action}")
+        }
+        bash.apple(script)
+        return "Done"
+    }
+
+    class Input(
+        @InputParamDescription("Action to perform: next, previous, play, pause, playpause, brightness_up, brightness_down, volume_up, volume_down")
+        val action: String
+    )
+}
