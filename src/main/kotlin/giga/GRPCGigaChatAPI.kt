bf431d2d9830c6dca4af6e58b0b4b1f0ecf11208
Artur Dumchev <arturdumchev@yandex.ru>
2025-08-12 23:35:04 +0300

    Handle custom root certificates for gRPC client (#50)
    * Configure gRPC client to load custom root certificates
    
    * Fix getting resources

--- src/main/kotlin/giga/GRPCGigaChatAPI.kt
+++ src/main/kotlin/giga/GRPCGigaChatAPI.kt
@@ -3,9 +3,12 @@ package com.dumch.giga
 import gigachat.v1.ChatServiceGrpcKt
 import gigachat.v1.Gigachatv1
 import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
 import io.grpc.Metadata
 import org.slf4j.LoggerFactory
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext
+import java.io.File
 
 /**
  * Simple gRPC client for GigaChat ChatService.
@@ -14,8 +17,8 @@ class GRPCGigaChatAPI(private val auth: GigaAuth) {
     private val l = LoggerFactory.getLogger(GRPCGigaChatAPI::class.java)
 
     private val channel: ManagedChannel =
-        ManagedChannelBuilder.forAddress("gigachat.devices.sberbank.ru", 443)
-            .useTransportSecurity()
+        NettyChannelBuilder.forAddress("gigachat.devices.sberbank.ru", 443)
+            .sslContext(loadSslContext())
             .build()
 
     private val stub: ChatServiceGrpcKt.ChatServiceCoroutineStub =
@@ -94,10 +97,18 @@ class GRPCGigaChatAPI(private val auth: GigaAuth) {
     companion object {
         val INSTANCE = GRPCGigaChatAPI(GigaAuth)
     }
+
+    private fun loadSslContext(): SslContext {
+        val certPath = "certs/russiantrustedca.pem"
+        val stream = Thread.currentThread().contextClassLoader.getResourceAsStream(certPath)
+                ?: throw IllegalStateException("Certificate not found: $certPath")
+        stream.use { ins ->
+            return GrpcSslContexts.forClient().trustManager(ins).build()
+        }
+    }
 }
 
 suspend fun main() {
-    println(System.getenv("GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"))
     val api = GRPCGigaChatAPI.INSTANCE
     val result = api.message(GigaRequest.Chat(
         model = "GigaChat-Pro",
