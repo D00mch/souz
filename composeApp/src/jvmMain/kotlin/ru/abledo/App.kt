8b17e7fc4bf155077c26dbe2710fb6890d70a389
dumch <arturdumchev@yandex.ru>
2025-11-25 00:56:15 +0300

    Open settings when no keys; bind main and settings screens with each other

--- composeApp/src/jvmMain/kotlin/ru/abledo/App.kt
+++ composeApp/src/jvmMain/kotlin/ru/abledo/App.kt
@@ -2,13 +2,38 @@ package ru.abledo
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import org.kodein.di.compose.localDI
+import org.kodein.di.instance
 import ru.abledo.ui.AppTheme
 import ru.abledo.ui.main.MainScreen
+import ru.abledo.ui.settings.SettingsScreen
+import ru.abledo.db.KeysProvider
 
 @Composable
 @Preview
 fun App() {
-    AppTheme {
-        MainScreen()
+    val di = localDI()
+    val keysProvider: KeysProvider by di.instance()
+    val shouldStartInSettings = remember(keysProvider) {
+        keysProvider.gigaChatKey.isNullOrEmpty() || keysProvider.saluteSpeechKey.isNullOrEmpty()
     }
+    var currentScreen by remember(shouldStartInSettings) {
+        mutableStateOf(if (shouldStartInSettings) Screen.Settings else Screen.Main)
+    }
+
+    AppTheme {
+        when (currentScreen) {
+            Screen.Main -> MainScreen(onOpenSettings = { currentScreen = Screen.Settings })
+            Screen.Settings -> SettingsScreen(onClose = { currentScreen = Screen.Main })
+        }
+    }
+}
+
+private enum class Screen {
+    Main,
+    Settings,
 }
