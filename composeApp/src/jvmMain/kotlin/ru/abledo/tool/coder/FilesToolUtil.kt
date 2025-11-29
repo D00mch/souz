801de9f4ebd157e310c20aa13e36537dc225d237
dumch <arturdumchev@yandex.ru>
2025-11-29 18:52:45 +0300

    Fix safe path definition

--- composeApp/src/jvmMain/kotlin/ru/abledo/tool/coder/FilesToolUtil.kt
+++ composeApp/src/jvmMain/kotlin/ru/abledo/tool/coder/FilesToolUtil.kt
@@ -6,17 +6,20 @@ import java.io.IOException
 import java.io.InputStream
 
 object FilesToolUtil {
-    private val projectRoot = File(".").canonicalFile
+    private val homeDirectory = File(System.getProperty("user.home")).canonicalFile
 
+    /**
+     * Generally, we don't want Agent to mess around /
+     */
     fun isPathSafe(file: File): Boolean {
         val canonicalPath = file.canonicalFile
-        return canonicalPath.startsWith(projectRoot)
+        return canonicalPath.startsWith(homeDirectory)
     }
 
     @Throws(BadInputException::class)
     fun requirePathIsSave(file: File) {
         if (!isPathSafe(file)) {
-            throw BadInputException("Access denied: File path must be within project directory")
+            throw BadInputException("Access denied: File path must be within the home directory")
         }
     }
 
