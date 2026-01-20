d8df7df166ef523bdb0e1048118a44c029aaa589
dumch <arturdumchev@yandex.ru>
2026-01-20 23:37:09 +0300

    Add api to queue jobs in audio

--- composeApp/src/jvmMain/kotlin/ru/gigadesk/audio/AudioPlay.kt
+++ composeApp/src/jvmMain/kotlin/ru/gigadesk/audio/AudioPlay.kt
@@ -1,5 +1,14 @@
+@file:OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+
 package ru.gigadesk.audio
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 import java.io.File
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.LineEvent
@@ -14,12 +23,27 @@ class Say {
     private var sayProcess: Process? = null
     private val random = Random()
 
+    private val jobs = ArrayList<Job>()
+    private val managingScope = CoroutineScope(SupervisorJob() + newSingleThreadContext("say-manage"))
+    private val voiceScope = CoroutineScope(SupervisorJob() + newSingleThreadContext("say"))
+
     /** Global Voice command. Only one speech is possible at the time. Use [stopPlayText] to stop the current one */
     fun playText(text: String, speed: Int = ConfigStore.get(SPEED_KEY, DEFAULT_SPEED)) {
         stopPlayText()
         val saveEnding = "$text "
         sayProcess = ProcessBuilder("say", "-r", "$speed", "--", saveEnding).start()
         sayProcess?.waitFor()
+    }
+
+    fun queue(text: String, speed: Int = ConfigStore.get(SPEED_KEY, DEFAULT_SPEED)) = managingScope.launch {
+        val j = voiceScope.launch { playText(text, speed) }
+        jobs.add(j)
+    }
+
+    fun clearQueue() = managingScope.launch {
+        jobs.forEach { it.cancel() }
+        jobs.clear()
+        stopPlayText()
     }
 
     /** Stops the speech started by [playText] */
