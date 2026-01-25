272265e10e06fb9d32e6a4948a91fca911fda801
Artur Dumchev <arturdumchev@yandex.ru>
2026-01-25 20:20:56 +0300

    Remove legacy GigaAgent

--- composeApp/src/jvmMain/kotlin/ru/gigadesk/TextMain.kt
+++ composeApp/src/jvmMain/kotlin/ru/gigadesk/TextMain.kt
@@ -5,26 +5,18 @@ import kotlinx.coroutines.flow.flow
 import org.kodein.di.DI
 import org.kodein.di.instance
 import org.slf4j.LoggerFactory
-import ru.gigadesk.agent.nodes.NodesClassification
-import ru.gigadesk.db.DesktopInfoRepository
+import ru.gigadesk.agent.GraphBasedAgent
 import ru.gigadesk.di.mainDiModule
-import ru.gigadesk.giga.GigaAgent
-import ru.gigadesk.giga.GigaRestChatAPI
 
 private val logAgent = LoggerFactory.getLogger("Agent")
 
 suspend fun main() {
     val di = DI.invoke { import(mainDiModule) }
-    val desktopInfoRepo: DesktopInfoRepository by di.instance()
-    val nodesClassification: NodesClassification by di.instance()
-    val gigaRestChatAPI: GigaRestChatAPI by di.instance()
-    val agent = GigaAgent.instance(
-        userInputFlow(),
-        gigaRestChatAPI,
-        desktopInfoRepo,
-        nodesClassification,
-    )
-    agent.run().collect { text -> logAgent.info(text) }
+    val agent: GraphBasedAgent by di.instance()
+    userInputFlow().collect { input ->
+        val response = agent.execute(input)
+        logAgent.info(response)
+    }
 }
 
 private fun userInputFlow(): Flow<String> = flow {
