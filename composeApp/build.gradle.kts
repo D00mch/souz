946257fa7e58cc6b13a84265d04cfa7d8e5637b0
dumch <arturdumchev@yandex.ru>
2026-03-15 03:57:59 +0300

    Revert "Update gradle version"
    This reverts commit 1ccf9beb6cd28c47b0160a49988b201226bf1ac2.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -197,7 +197,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Pkg)
             packageName = distributionPackageName
-            packageVersion = "1.0.5"
+            packageVersion = "1.0.4"
             // Compose copies app resources from <root>/common + OS/target subfolders.
             appResourcesRootDir.set(preparedAppResourcesDir)
 
