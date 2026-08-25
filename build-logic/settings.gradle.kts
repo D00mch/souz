48b812a17f72429313602b440edad74f74144bb3
Artur Dumchev <arturdumchev@yandex.ru>
2026-08-25 23:08:25 +0300

    Bypass JetBrains Gradle plugin markers (#695)
    * build: bypass JetBrains plugin markers
    
    * Fix defaulted plugin resolution versions

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -8,6 +8,15 @@ pluginManagement {
         mavenCentral()
     }
 
+    resolutionStrategy {
+        eachPlugin {
+            val version = target.version ?: requested.version ?: return@eachPlugin
+            if (requested.id.id == "org.jetbrains.kotlin.jvm") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:$version")
+            }
+        }
+    }
+
     plugins {
         id("org.jetbrains.kotlin.jvm") version "2.4.10"
     }
